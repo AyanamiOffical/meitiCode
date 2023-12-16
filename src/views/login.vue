00cacc71dc89d6c50dcd0b21e2b7f1496b358425
sherlock <3222907895@QQ.com>
2023-12-17 01:09:41 +0800

    final version

--- src/views/login.vue
+++ src/views/login.vue
@@ -171,7 +171,7 @@ const onSubmit = async () => {
       @apply  justify-center font-bold text-5xl text-light-100 mb-4 ;
     }
     .smallTitle{
-      @apply text-light-500 font-bold
+      @apply text-light-5000 font-bold
     }
   }
   .right {
@@ -203,10 +203,10 @@ const onSubmit = async () => {
   background-color: rgba(255, 255, 255, 0.15);
   position: absolute;
   bottom: -160px;
-  /* Execute animation: animation name duration linear infinite */
+  /* 执行动画：动画名 时长 线性 无限次播放*/
   animation: squres 2s linear infinite ;
 }
-/* Set different positions, sizes, animation delays, and durations, and background colors for each square */
+/* 为每一个方块设置不同的位置、大小、动画延迟时间、动画时长、背景色  */
 .bg-squares li:nth-child(1){
   left: 10%;
 }
@@ -214,14 +214,65 @@ const onSubmit = async () => {
   left: 20%;
   width: 80px;
   height: 80px;
-  /* Animation delay */
+  /* 动画延迟时间 */
   animation-delay:2s;
-  /* Animation duration */
+  /* 动画时长 */
   animation-duration: 17s;
 }
 .bg-squares li:nth-child(3){
   left: 25%;
   animation-delay: 4s;
 }
-
+.bg-squares li:nth-child(4){
+  left: 40%;
+  width: 60px;
+  height: 60px;
+  background-color:rgba(255, 255, 255, 0.25) ;
+  animation-duration: 22s;
+}
+.bg-squares li:nth-child(5){
+  left: 70%;
+}
+.bg-squares li:nth-child(6){
+  left: 80%;
+  width: 120px;
+  height:120px;
+  background-color:rgba(255, 255, 255, 0.2) ;
+  animation-delay: 3s;
+}
+.bg-squares li:nth-child(7){
+  left: 32%;
+  width: 160px;
+  height: 160px;
+  animation-delay: 7s;
+}
+.bg-squares li:nth-child(8){
+  left: 55%;
+  width: 20px;
+  height: 20px;
+  animation-delay: 15s;
+  animation-duration: 40s;
+}
+.bg-squares li:nth-child(9){
+  left: 25%;
+  width: 10px;
+  height: 10px;
+  background-color: rgba(255, 255, 255, 0.3);
+  animation-delay: 2s;
+  animation-duration: 40s;
+}
+.bg-squares li:nth-child(10){
+  left: 90%;
+  width: 160px;
+  height: 160px;
+  animation-delay: 11s;
+}
+@keyframes squres{
+  0%{
+    transform: translateY(0);
+  }
+  100%{
+    transform: translateY(-120vh) rotate(600deg);
+  }
+}
 </style>
